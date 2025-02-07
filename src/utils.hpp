35b95d3da23c8ef9dc74c43e5364e49e152fce61
sh-r0 <imatti1337@gmail.com>
2025-02-07 17:39:20 +0100

    fixed camera bug

--- src/utils.hpp
+++ src/utils.hpp
@@ -3,6 +3,11 @@
 #include <cmath>
 #include "gameTypes.hpp"
 
+template<typename T> 
+inline T abs(T _x) {
+    return _x < 0 ? -_x : _x;
+}
+
 template<typename T>
 void normalizeVec(std::array<T, 2>& _vec) {
 	double vecLength = sqrt(_vec[0] * _vec[0] + _vec[1] * _vec[1]);
